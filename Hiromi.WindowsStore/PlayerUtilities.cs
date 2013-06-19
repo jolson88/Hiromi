9516e2bdb3c11e08c4ba12c828cb84371c6d54f8
jolson88 <jolson88@outlook.com>
2013-06-19 10:53:45 -0700

    Added gen of random player names

---
+++ Hiromi.WindowsStore/PlayerUtilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiromi
+{
+    public static class PlayerUtilities
+    {
+        public static string GetRandomPlayerName()
+        {
+            var playerName = "Player";
+
+            var guidBytes = Guid.NewGuid().ToByteArray();
+            for (int i = 0; i < 5; i++)
+            {
+                playerName += guidBytes[i].ToString();
+            }
+
+            return playerName;
+        }
+    }
+}
